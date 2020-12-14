1a3dfb46f47014535fdfb3a8c731d5a1e10b0354
microdream <liuyanggang12345@qq.com>
2020-12-14 10:52:03 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <van-nav-bar title="标题" left-text="返回" right-text="按钮" left-arrow @click-left="onClickLeft"
+                 @click-right="onClickRight"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
